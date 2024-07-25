998e0fe5dc788ec3687f189210ed2091049b4ba1
Melpasani <mepsanchez@est.istg.edu.ec>
2024-07-25 04:05:56 -0500

    cambios en gestion de proveedores

--- model/gestion_proveedores.py
+++ model/gestion_proveedores.py
@@ -5,6 +5,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../app'))
 
 import tkinter as tk
+from PIL import Image, ImageTk
 from tkinter import ttk, messagebox, simpledialog
 from proveedores import Proveedores
 
@@ -14,33 +15,60 @@ class GestionProveedores:
         self.root = root
         self.root.title("Proveedores - Ferretería Argamasa")  # Establecer el título de la ventana
         self.root.geometry("800x600")  # Establecer el tamaño de la ventana principal
+        self.root.resizable(False, False)
+        self.root.iconbitmap("recursos/LOGO.ico")
 
         # Crear una instancia de la clase Proveedores
         self.proveedores = Proveedores()
 
-        # Configuración del marco principal
-        self.frame = tk.Frame(self.root)
-        self.frame.pack(fill=tk.BOTH, expand=True)  # Empaquetar el marco para llenar el espacio disponible
+        # Crear un canvas para colocar elementos gráficos
+        self.canvas = tk.Canvas(self.root, width=800, height=600)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
 
-        # Título de la ventana
-        self.titulo = tk.Label(self.frame, text="Proveedores", font=("Arial", 24))
-        self.titulo.pack(pady=10)  # Añadir un espaciado vertical
+        # Crear el marco para los controles
+        self.frame = tk.Frame(self.canvas, bg="")
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER, relwidth=0.9, relheight=0.9)
+
+        # Cargar y establecer la imagen de fondo
+        self.cargar_imagen_fondo()
+
+        # Título de la ventana directamente en el canvas
+        self.canvas.create_text(400, 50, text="PROVEEDORES", font=("Arial", 30, "bold"), fill="black", anchor=tk.CENTER)
+
+        self.crear_botones()
 
         # Contenedor de botones
         self.botones = tk.Frame(self.frame)
         self.botones.pack(pady=10)  # Añadir un espaciado vertical
 
-        # Botón para agregar proveedor
-        self.btn_agg = tk.Button(self.botones, text="AGREGAR PROVEEDOR", command=self.agregar_proveedor)
-        self.btn_agg.grid(row=0, column=0, padx=10)  # Añadir un espaciado horizontal
+        # Estilo a Tabla
+        estilo = ttk.Style()
+        estilo.theme_use("default")
+        estilo.configure("Treeview", 
+                         background="#f0f0f0", 
+                         foreground="black", 
+                         rowheight=25, 
+                         fieldbackground="white", 
+                         bordercolor="black",  
+                         borderwidth=1,
+                         font=("Arial", 8))
+        estilo.map("Treeview", 
+                   background=[('selected', '#d3d3d3')], 
+                   foreground=[('selected', 'black')])
+        
+        estilo.configure("Treeview.Heading", font=("Arial", 9, "bold"), bordercolor="black", borderwidth=1)
 
-        # Botón para editar proveedor
-        self.btn_edit = tk.Button(self.botones, text="EDITAR PROVEEDOR", command=self.editar_proveedor)
-        self.btn_edit.grid(row=0, column=1, padx=10)  # Añadir un espaciado horizontal
-
-        # Botón para eliminar proveedor
-        self.btn_eliminar = tk.Button(self.botones, text="ELIMINAR PROVEEDOR", command=self.eliminar_proveedor)
-        self.btn_eliminar.grid(row=0, column=2, padx=10)  # Añadir un espaciado horizontal
+        estilo.layout("Treeview", [
+            ('Treeview.treearea', {'sticky': 'nswe'}),
+            ('Treeview.padding', {'sticky': 'nswe'}),
+            ('Treeview.cell', {'sticky': 'nswe', 'children': [
+                ('Treeitem.padding', {'sticky': 'nswe', 'children': [
+                    ('Treeitem.indicator', {'side': 'left', 'sticky': ''}),
+                    ('Treeitem.image', {'side': 'left', 'sticky': ''}),
+                    ('Treeitem.text', {'side': 'left', 'sticky': ''}),
+                ]}),
+            ]}),
+        ])
 
         # Crear una tabla (Treeview) para mostrar los proveedores
         self.tree = ttk.Treeview(self.frame, columns=("id_proveedor", "nombre_proveedor", "direccion", "contacto", "email"), show='headings')
@@ -49,9 +77,57 @@ class GestionProveedores:
         self.tree.heading("direccion", text="Direccion")
         self.tree.heading("contacto", text="Contacto")
         self.tree.heading("email", text="Email")
+                # Configuración de las columnas 
+        self.tree.column("id_proveedor", width=100, anchor='center')
+        self.tree.column("nombre_proveedor", width=150, anchor='center')
+        self.tree.column("direccion", width=100, anchor='center')
+        self.tree.column("contacto", width=80, anchor='center')
+        self.tree.column("email", width=190, anchor='center')
 
-        self.tree.pack(fill=tk.BOTH, expand=True)  # Empaquetar la tabla para llenar el espacio disponible
+        self.tree.pack(pady=(120, 10), fill=tk.BOTH, expand=False, padx=10)  # Empaquetar la tabla para llenar el espacio disponible
+
+        self.mostrar_logo()
         self.cargar_datos()  # Cargar datos en la tabla al iniciar
+
+    def cargar_imagen_fondo(self):
+        try:
+            self.bg_image = Image.open("recursos/FONDO.png")
+            self.bg_photo = ImageTk.PhotoImage(self.bg_image)
+            self.canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_photo, tags="bg_image")
+        except Exception as e:
+            print(f"Error al cargar la imagen de fondo: {e}")
+
+    def actualizar_imagen_fondo(self, event=None):
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+        
+        resized_image = self.bg_image.resize((canvas_width, canvas_height), Image.Resampling.LANCZOS)
+        self.bg_photo = ImageTk.PhotoImage(resized_image)
+        
+        self.canvas.delete("bg_image")
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_photo, tags="bg_image")
+
+    def crear_botones(self):
+        # Botón para agregar proveedor
+        self.btn_agg = tk.Button(self.canvas, text="AGREGAR PROVEEDOR", command=self.agregar_proveedor, width=19, height=2, font=("Arial", 10, "bold"), bg="#DCD2F0", cursor="hand2")
+        self.canvas.create_window(200, 130, window=self.btn_agg) # Añadir un espaciado horizontal 
+        # Botón para editar proveedor
+        self.btn_edit = tk.Button(self.canvas, text="EDITAR PROVEEDOR", command=self.editar_proveedor, width=19, height=2, font=("Arial", 10, "bold"), bg="#DCD2F0", cursor="hand2")
+        self.canvas.create_window(400, 130, window=self.btn_edit)  # Añadir un espaciado horizontal
+        # Botón para eliminar proveedor
+        self.btn_eliminar = tk.Button(self.canvas, text="ELIMINAR PROVEEDOR", command=self.eliminar_proveedor, width=19, height=2, font=("Arial", 10, "bold"), bg="#DCD2F0", cursor="hand2")
+        self.canvas.create_window(600, 130, window=self.btn_eliminar)  # Añadir un espaciado horizontal
+
+    def mostrar_logo(self):
+        try:
+            # Cargar la imagen del logo
+            self.logo_image = Image.open("recursos/ARGAMASA_logo.png")
+            self.logo_photo = ImageTk.PhotoImage(self.logo_image)
+
+            # Colocar el logo debajo del widget Treeview
+            self.canvas.create_image(400, 525, image=self.logo_photo, anchor=tk.CENTER)
+        except Exception as e:
+            print(f"Error al cargar el logo: {e}")
 
     def cargar_datos(self):
         # Limpiar datos actuales de la tabla
